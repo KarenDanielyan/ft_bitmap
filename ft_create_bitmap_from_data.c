be68eb42db5db4d21358cf8cf0cb1982c53388fe
Karen Danielyan <karendanielyan66@gmail.com>
2024-05-31 02:50:00 +0400

    Moving ft_create_bitmap_from_data() to a separate file

--- ft_create_bitmap_from_data.c
+++ ft_create_bitmap_from_data.c
@@ -1,41 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_create_bitmap.c                                 :+:      :+:    :+:   */
+/*   ft_create_bitmap_from_data.c                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 02:28:19 by kdaniely          #+#    #+#             */
-/*   Updated: 2024/05/31 00:11:49 by kdaniely         ###   ########.fr       */
+/*   Updated: 2024/05/31 02:36:06 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_bitmap.h"
-
-static void	fill_file_header(t_ftbitmap *bitmap, \
-	uint32_t width, uint32_t height);
-static void	fill_info_header(t_ftbitmap *bitmap, \
-	uint32_t width, uint32_t height);
-
-t_ftbitmap	*ft_create_bitmap(uint32_t width, uint32_t height)
-{
-	t_ftbitmap	*bitmap;
-
-	bitmap = (t_ftbitmap *)malloc(sizeof(t_ftbitmap));
-	if (!bitmap)
-		return (NULL);
-	fill_file_header(bitmap, width, height);
-	fill_info_header(bitmap, width, height);
-	bitmap->fh.bf_size = BITMAP_HEADER_SIZE + bitmap->ih.bi_size_image;
-	bitmap->data = (unsigned char *)malloc(bitmap->ih.bi_size_image);
-	if (!bitmap->data)
-	{
-		free(bitmap);
-		return (NULL);
-	}
-	memset(bitmap->data, 255, bitmap->ih.bi_size_image);
-	return (bitmap);
-}
 
 static void	fill_file_header(t_ftbitmap *bitmap, \
 	uint32_t width, uint32_t height)
@@ -47,8 +22,6 @@ static void	fill_file_header(t_ftbitmap *bitmap, \
 	bitmap->fh.bf_reserved2 &= 0;
 	bitmap->fh.bf_off_bits = BITMAP_HEADER_SIZE;
 }
-
-/* stride = ((((biWidth * biBitCount) + 31) & ~31) >> 3); */
 
 static void	fill_info_header(t_ftbitmap *bitmap, \
 	uint32_t width, uint32_t height)
@@ -64,4 +37,57 @@ static void	fill_info_header(t_ftbitmap *bitmap, \
 	bitmap->ih.bi_y_pels_per_meter = FT_BITMAP_Y_PIXELS_PER_METER;
 	bitmap->ih.bi_clr_used = 0;
 	bitmap->ih.bi_clr_important = 0;
+}
+
+/* Fill bitmap data with pixel information. Consider padding too.*/
+/* BGRA -> RGB */
+/*
+ * Little Endian: 00 00 FF 00 <- 00FF0000
+*/
+static void	fill_data(t_ftbitmap *bitmap, unsigned char *data)
+{
+	int32_t			i;
+	int32_t			j;
+	int32_t			height;
+	unsigned int	color;
+
+	i = 0;
+	height = bitmap->ih.bi_height;
+	if (height < 0)
+		height = -height;
+	while (i < height)
+	{
+		j = 0;
+		while (j < bitmap->ih.bi_width)
+		{
+			color = \
+			*((unsigned int *)(data + (i * bitmap->ih.bi_width + j) * 4));
+			ft_bitmap_set_pixel(bitmap, j, i, color);
+			j ++;
+		}
+		i ++;
+	}
+}
+
+/* Read pixel information from data. pixels in data are BGRA. */
+t_ftbitmap	*ft_create_bitmap_from_data(uint32_t width, uint32_t height, \
+	unsigned char *data)
+{
+	t_ftbitmap	*bitmap;
+
+	bitmap = (t_ftbitmap *)malloc(sizeof(t_ftbitmap));
+	if (!bitmap)
+		return (NULL);
+	fill_file_header(bitmap, width, height);
+	fill_info_header(bitmap, width, height);
+	bitmap->fh.bf_size = BITMAP_HEADER_SIZE + bitmap->ih.bi_size_image;
+	bitmap->data = (unsigned char *)malloc(bitmap->ih.bi_size_image);
+	if (!bitmap->data)
+	{
+		free(bitmap);
+		return (NULL);
+	}
+	memset(bitmap->data, 0, bitmap->ih.bi_size_image);
+	fill_data(bitmap, data);
+	return (bitmap);
 }
